8c336040aa5b78f8b183f8725c8f30a75adfa522
ritik <ritikbhatt020@gmail.com>
2024-06-21 20:54:28 +0530

    Feat: Added Linkedin Profile views

--- src/services/linkedinService.js
+++ src/services/linkedinService.js
@@ -3,11 +3,11 @@ const { ReclaimServiceResponse } = require('../utils/reclaimServiceResponse');
 
 exports.processLinkedInData = async (proof, providerName) => {
   const extractedParameters = JSON.parse(proof[0].claimData.context).extractedParameters;
-  const postImpressions = extractedParameters.Post_Impressions;
+  const profileViews = extractedParameters.Profile_Views;
 
   const username = proof[0].claimData.owner;
 
-  console.log(`Post Impressions for user ${username}: ${postImpressions}`);
+  console.log(`Profile Views for user ${username}: ${profileViews}`);
 
   const lastUpdateTimeStamp = proof[0].claimData.timestampS;
 
@@ -15,7 +15,7 @@ exports.processLinkedInData = async (proof, providerName) => {
     providerName,
     lastUpdateTimeStamp,
     username,
-    parseInt(postImpressions, 10),
+    parseInt(profileViews, 10),
     proof[0]
   );
 };
