4142c4de3da07f3d5775e53d34b0e92c255b69bd
BRAGADEESH_V <bragadeesh2005@gmail.com>
2024-06-21 16:25:06 +0530

    added strava_integration

--- src/services/reclaimService.js
+++ src/services/reclaimService.js
@@ -3,6 +3,7 @@ const { Reclaim } = require('@reclaimprotocol/js-sdk')
 const { RECLAIM_PROVIDER_ID, RECLAIM_APP_ID } = require('../utils/constants')
 const { processTwitterData } = require('./twitterService')
 const { processGitHubData } = require('./githubService')
+const { processStravaIntegrationData } = require('./stravaIntegrationService')
 
 exports.signWithProviderID = async (userId, providerId) => {
   const providerName = RECLAIM_PROVIDER_ID[providerId]
@@ -48,6 +49,12 @@ const handleReclaimSession = async (userId, reclaimClient, providerName) => {
             break
           case 'GITHUB_ACCOUNT_VERIFICATION':
             processedData = await processGitHubData(proof, providerName)
+            break
+          case 'STRAVA_SERVICE':
+            processedData = await processStravaIntegrationData(
+              proof,
+              providerName
+            )
             break
           default:
             throw new Error(`No handler for provider: ${providerName}`)
