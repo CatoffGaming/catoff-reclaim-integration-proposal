48b223bd2dc98c787d4b250f210c0c00763bc9a9
ritik <ritikbhatt020@gmail.com>
2024-07-24 14:00:58 +0530

    Feat: Added Groww Available Balance Verification

--- src/services/reclaimService.js
+++ src/services/reclaimService.js
@@ -3,7 +3,7 @@ const { Reclaim } = require('@reclaimprotocol/js-sdk')
 const { RECLAIM_PROVIDER_ID, RECLAIM_APP_ID } = require('../utils/constants')
 const { processTwitterData } = require('./twitterService')
 const { processGitHubData } = require('./githubService')
-const { processGrowwAccountData } = require('./GrowwAccountDataService')
+const { processGrowwAccountData } = require('./GrowwAvailableBalanceService')
 
 exports.signWithProviderID = async (userId, providerId) => {
   const providerName = RECLAIM_PROVIDER_ID[providerId]
@@ -15,10 +15,10 @@ exports.signWithProviderID = async (userId, providerId) => {
   )
 
   try {         
-    const reclaimClient = new Reclaim.ProofRequest('0x4a5454dD6ff5D5718822251ed27aA797E88c53C9')
-    await reclaimClient.buildProofRequest('9d4785ab-f71e-4c4c-9b90-32573da992a6')
+    const reclaimClient = new Reclaim.ProofRequest('0xcEb6310D69a18f09bFd2365be833937334Cb424E')
+    await reclaimClient.buildProofRequest('1faf8c2a-cc18-4150-a7cf-66320971d0fb')
     reclaimClient.setSignature(
-      await reclaimClient.generateSignature('0x802f4fe1054cee73d042ff7673344075b60cb161e6d0c26fe9cb5568228d496c')
+      await reclaimClient.generateSignature('0xf1a58819d01693375e57ee4949adf0eea50b3524ba504e35fd9ea2294045af83')
     )
     const { requestUrl: signedUrl } =
       await reclaimClient.createVerificationRequest()
